6da3efaed669d438ff103c823bed3ffe0188210d
Taku3939 <sakisaki200012@gmail.com>
2020-11-08 07:19:01 +0900

    update cofig

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,10 @@
 const root = "http://localhost:3010";
 const clientConfig = {
     root: root,
-    qr: root + "qr.png",
+    qr: root + "/qr.png",
     googleLogin: root + "/auth/google",
     twitterLogin: root + "/auth/twitter",
-    zip: root + "hoge",
+    zip: root + "/vll-live-app.zip",
 }
 
 export default clientConfig;
